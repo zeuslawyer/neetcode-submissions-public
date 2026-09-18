e91055bcbb49d1fce8477b162273b01a76257e3b
Zubin <zeuslawyer@users.noreply.github.com>
2026-09-18 21:44:22 +1000

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if len(image) == 0:
+            return [[]]
+
+        original = image[sr][sc]
+        if color == original:
+            return image
+
+        
+        # bfs
+        q = deque()
+        q.append((sr, sc))
+        
+        while q:
+            r, c = q.popleft()
+            image[r][c] = color
+
+            neighbours = self.valid_neighbours(r,c, image, original)
+            for n in neighbours:
+                if not n:
+                    continue
+
+                q.append(n)
+
+
+        return image
+
+
+    def valid_neighbours(self, r, c, image, original):
+        valids = []
+
+        left, right, up, down = None, None, None,None
+        
+        # left
+        if c -1 >= 0 and  image[r][c-1] == original:
+                valids.append((r, c-1))
+        
+        # right
+        if c+1 < len(image[0]) and image[r][c+1] == original:
+            valids.append((r, c+1))
+
+        # up
+        if r -1 >= 0 and image[r-1][c] == original:
+            valids.append((r-1, c ))
+    
+
+        # down
+        if r+1 < len(image) and image[r+1][c] == original:
+            valids.append((r+1, c))
+        
+        return valids
+
+
+        
+
+
+
+
+        
