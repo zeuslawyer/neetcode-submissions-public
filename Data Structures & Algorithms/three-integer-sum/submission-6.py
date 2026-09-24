35bbcb1fa05821d1f8eee750ff955b9403127b6c
Zubin <zeuslawyer@users.noreply.github.com>
2026-09-24 18:53:41 +1000

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        if len(nums) == 0: return res
+
+        visited = set()
+        nums.sort()
+
+        for i, num in enumerate(nums):
+            # since it sorted, dupes will be contiguous
+            if i > 0 and nums[i-1] == num:
+                continue
+            
+            l, r = i+1, len(nums)-1
+            while l < r:
+                sum = num + nums[l] + nums[r]
+                if sum == 0:
+                    res.append([num, nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                    while nums[l] == nums[l-1] and l < r:
+                        l+=1
+                elif sum < 0:
+                    # need to increase sum so we move towards the right of the sorted array
+                    l+=1
+                else:
+                    # need to decrease sum so we move towards the left of the sorted array
+                    r -= 1
+        
+        return res 
+
